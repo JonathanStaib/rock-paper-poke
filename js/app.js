01872d7fea44e13e1c056326df8677b25fba3eff
Lewis Benson <tm.lewis.benson@gmail.com>
2022-10-06 16:06:21 -0400

    refactor new hand proto, and add comments

--- js/app.js
+++ js/app.js
@@ -65,11 +65,10 @@ const playersHand = new PlayerDeck();
 /* PROTOTYPE METHODS */
 
 PlayerDeck.prototype.newHand = function () {
-  pickRandomType('fire');
-  pickRandomType('ice');
-  pickRandomType('electric');
-  pickRandomType('ground');
-  pickRandomType('grass');
+  let typesByElement = Object.keys(types); // referencing the object called types on line 44 for key names as a string
+  for (let elementFromTypesObject of typesByElement) {
+    pickRandomType(elementFromTypesObject); // insert the keyname (as a string) into the pickRandomType function
+  }
 };
 
 /* DOM MANIPULATION */
@@ -91,22 +90,24 @@ let iceName = document.querySelector('#ice');
 let opponentName = document.querySelector('#opponent-name');
 
 /* UTILITY FUNCTIONS */
-1;
-function randomPokemon(element) {
+
+function randomPokemon(element) { // element is referring to the arrays on lines 36/40
   return Math.floor(Math.random() * element.length);
 }
 function storeToLocal() {
   let uniquePokemonNames = [];
   let uniquePokemonUrls = [];
+  // the flat method grabs all arrays nested inside an array and turns them into 1 big array
   let uniquePokemonFlat = [...new Set(pokemonSpotted.flat())]; // remove duplicates from array
-  for (let i = 1; i < uniquePokemonFlat.length; i += 2) {
+  for (let i = 1; i < uniquePokemonFlat.length; i += 2) {// starting at index 1, grab all odd indexs and push into names
     uniquePokemonNames.push(uniquePokemonFlat[i]);
   }
 
-  for (let i = 0; i < uniquePokemonFlat.length; i += 2) {
+  for (let i = 0; i < uniquePokemonFlat.length; i += 2) { // start at index 0 and grab all even indexes
     uniquePokemonUrls.push(uniquePokemonFlat[i]);
   }
 
+  /* Grab all data for the pokedex and place it in local storage */
   localStorage.setItem(
     'pokemonSpottedNames',
     JSON.stringify(uniquePokemonNames)
@@ -243,45 +244,45 @@ which element beats which */
 function winChecker(usersChoice) {
   computersType = computer.alt; //set computer type to whats stored in the pokemon img alt
   switch (computersType) {
-  case 'fire':
-    if (usersChoice === 'ground' || usersChoice === 'electric') {
-      playerWins(usersChoice);
-    } else {
-      playerLoss(usersChoice);
-    }
-    break;
+    case 'fire':
+      if (usersChoice === 'ground' || usersChoice === 'electric') {
+        playerWins(usersChoice);
+      } else {
+        playerLoss(usersChoice);
+      }
+      break;
 
-  case 'ground':
-    if (usersChoice === 'ice' || usersChoice === 'grass') {
-      playerWins(usersChoice);
-    } else {
-      playerLoss(usersChoice);
-    }
-    break;
-  case 'grass':
-    if (usersChoice === 'fire' || usersChoice === 'ice') {
-      playerWins(usersChoice);
-    } else {
-      playerLoss(usersChoice);
-    }
-    break;
+    case 'ground':
+      if (usersChoice === 'ice' || usersChoice === 'grass') {
+        playerWins(usersChoice);
+      } else {
+        playerLoss(usersChoice);
+      }
+      break;
+    case 'grass':
+      if (usersChoice === 'fire' || usersChoice === 'ice') {
+        playerWins(usersChoice);
+      } else {
+        playerLoss(usersChoice);
+      }
+      break;
 
-  case 'electric':
-    if (usersChoice === 'grass' || usersChoice === 'ground') {
-      playerWins(usersChoice);
-    } else {
-      playerLoss(usersChoice);
-    }
-    break;
-  case 'ice':
-    if (usersChoice === 'electric' || usersChoice === 'fire') {
-      playerWins(usersChoice);
-    } else {
-      playerLoss(usersChoice);
-    }
-    break;
-  default:
-    console.log('error');
+    case 'electric':
+      if (usersChoice === 'grass' || usersChoice === 'ground') {
+        playerWins(usersChoice);
+      } else {
+        playerLoss(usersChoice);
+      }
+      break;
+    case 'ice':
+      if (usersChoice === 'electric' || usersChoice === 'fire') {
+        playerWins(usersChoice);
+      } else {
+        playerLoss(usersChoice);
+      }
+      break;
+    default:
+      console.log('error');
   }
 }
 
